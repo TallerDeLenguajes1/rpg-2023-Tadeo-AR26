e1ad570d8cf1e480020c3e39c5f40e19b00da5ce
Tadeo-AR26 <alonsoruiztadeo26@gmail.com>
2023-06-21 10:42:44 -0300

    combate

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
 ﻿using System;
 using Espacio.Personaje;
 
+void Combate(int primero, int segundo){
+    while((ListaPersonaje[primero] > 0) && (ListaPersonaje[segundo] > 0)){
+        int dano = ((ListaPersonaje[primero].Destreza * ListaPersonaje[primero].Fuerza * ListaPersonaje[primero].Nivel) - (ListaPersonaje[segundo].Velocidad * ListaPersonaje[segundo].armadura))/500;
+        Console.WriteLine($"{ListaPersonaje[segundo].Apodo}RECIBE {dano} DE DAÑO");
+        ListaPersonaje[segundo].Salud -= dano;
+        Console.WriteLine($"Salud Restante: {ListaPersonaje[segundo].Salud}");
+        int dano = ((ListaPersonaje[segundo].Destreza * ListaPersonaje[segundo].Fuerza * ListaPersonaje[segundo].Nivel) - (ListaPersonaje[primero].Velocidad * ListaPersonaje[primero].armadura))/500;
+        Console.WriteLine($"{ListaPersonaje[primero].Apodo}RECIBE {dano} DE DAÑO");
+        ListaPersonaje[primero].Salud -= dano;
+        Console.WriteLine($"Salud Restante: {ListaPersonaje[primero].Salud}");
+    }
+    if(ListaPersonaje[primero] <= 0){
+        ListaPersonaje.Remove(ListaPersonaje[primero]);
+    }
+    if(ListaPersonaje[segundo] <= 0){
+        ListaPersonaje.Remove(ListaPersonaje[segundo]);
+    }
+}
+
 Personaje NuevoPersonaje;
 FabricaDePersonajes fp = new FabricaDePersonajes(); //Inicializo las funciones fabrica y pjson.
 PersonajesJson pjson = new PersonajesJson();
@@ -14,14 +33,14 @@ for(int i = 0; i<10; i++){
 Console.WriteLine(ListaPersonaje[1].Velocidad);
 
 while(ListaPersonaje.Count() != 1){
-    Random random = new Random();
+    Random random = new Random();h
     int primero = 0, segundo = 0;
     while(primero == segundo){
         primero = random.Next(0, ListaPersonaje.Count());
         segundo = random.Next(0, ListaPersonaje.Count());
     }
-    while((ListaPersonaje[Primero].Salud > 0) && (ListaPersonaje[Primero].Salud > 0)){
-        
+    if(ListaPersonaje[primero].Velocidad > ListaPersonaje[segundo].Velocidad){
+        Combate(primero, segundo);
     }
 }
 //pjson.GuardarPersonaje("save" ,ListaPersonaje); //guardar personajes
